7a02938e5bdb3566e8777a258f084b94b4000286
Martin Fournier <martinfournier@scolab.com>
2014-05-11 13:34:45 -0400

    Add zombie, copied from player.

---
+++ src/Zombie.ts
@@ -0,0 +1,19 @@
+﻿module Lowrezjam {
+
+    export class Zombie extends Phaser.Sprite {
+        game: Lowrezjam.Game;
+
+
+        constructor() {
+            this.game = window["game"];
+            super(this.game, 1, 1, 'player');
+            this.game.add.existing(this);
+            this.smoothed = false;
+
+            this.scale.x = this.game.gameScale;
+            this.scale.y = this.game.gameScale;
+
+        }
+    }
+
+}
